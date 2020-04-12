36d4eab5881f6ed222b363cd6f587388be0c69d5
arif-banai <banai.arif@gmail.com>
2020-04-11 23:55:42 -0400

    Added EventHandlers for conditions which violate the grace period rules

--- src/main/java/me/smeef/deathgraceperiod/DeathGracePeriod.java
+++ src/main/java/me/smeef/deathgraceperiod/DeathGracePeriod.java
@@ -2,14 +2,23 @@ package me.smeef.deathgraceperiod;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockState;
+import org.bukkit.block.data.BlockData;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.HashMap;
@@ -23,6 +32,7 @@ public final class DeathGracePeriod extends JavaPlugin implements Listener {
     private final String prefix = "[" + ChatColor.RED + "DeathGrace" + ChatColor.RESET + "] ";
 
     private HashMap<String, Long> playersOnGracePeriod;
+    private HashMap<String, Integer> playerGraceEndingTask;
 
     @Override
     public void onEnable() {
@@ -32,8 +42,8 @@ public final class DeathGracePeriod extends JavaPlugin implements Listener {
         this.invincibleTime = getConfig().getLong("invincibility-duration", 60L) * 20L;
 
         playersOnGracePeriod = new HashMap<String, Long>();
+        playerGraceEndingTask = new HashMap<String, Integer>();
         getServer().getPluginManager().registerEvents(this, this);
-
     }
 
     @Override
@@ -42,13 +52,77 @@ public final class DeathGracePeriod extends JavaPlugin implements Listener {
         HandlerList.unregisterAll((JavaPlugin) this);
     }
 
-    /** This handles the event where an entity, who is a player,
-     * receives damage. It negates all incoming damage if the player
-     * has recently died.
+    /**
+     * If a player places a block while in a grace period, end the grace period.
      *
-     * If the damage was caused by another player,
-     * send a message to the attacking player that
-     * their target is invulnerable.
+     * @param event The event where a Block is place by a Player
+     */
+    @EventHandler
+    public void onBlockPlaceEvent(final BlockPlaceEvent event) {
+        Player p = event.getPlayer();
+        String playerUUID = p.getUniqueId().toString();
+
+        if(playersOnGracePeriod.containsKey(playerUUID)) {
+            cancelGraceEndingTask(playerUUID);
+            p.sendMessage(prefix + "You are no longer invulnerable because you placed a block!");
+        }
+    }
+
+    /**
+     * If a player breaks a block while in a grace period, end the grace period.
+     *
+     * @param event The event where a Block is broken by a Player
+     */
+    @EventHandler
+    public void onBlockBreakEvent(final BlockBreakEvent event) {
+        Player p = event.getPlayer();
+        String playerUUID = p.getUniqueId().toString();
+
+        if(playersOnGracePeriod.containsKey(playerUUID)) {
+            cancelGraceEndingTask(playerUUID);
+            p.sendMessage(prefix + "You are no longer invulnerable because you broke a block!");
+        }
+    }
+
+    /**
+     * If a player is in a grace period, and interacts (right-clicks) on a block that
+     * has an inventory, or otherwise is not allowed to be interacted with during
+     * a grace period, that player's grace period is disabled.
+     *
+     * @param event The event where a Player interacts with something
+     */
+    @EventHandler
+    public void onPlayerInteractEvent(final PlayerInteractEvent event) {
+        Player p = event.getPlayer();
+        String playerUUID = p.getUniqueId().toString();
+
+        if(playerHasGracePeriod(playerUUID)) {
+            if(event.getAction() == Action.RIGHT_CLICK_BLOCK && !event.isBlockInHand()) {
+                Block clickedBlock = event.getClickedBlock();
+
+                if(clickedBlock != null) {
+                    BlockState blockState = clickedBlock.getState();
+                    BlockData blockData = clickedBlock.getBlockData();
+                    Material material = clickedBlock.getType();
+
+                    if(blockState instanceof InventoryHolder ||
+                            material == Material.ANVIL ||
+                            material == Material.CHIPPED_ANVIL ||
+                            material == Material.DAMAGED_ANVIL) {
+
+                        cancelGraceEndingTask(playerUUID);
+                        p.sendMessage(prefix + "You are no longer invulnerable because you interacted with a block!");
+                    }
+                }
+            }
+        }
+    }
+
+    /** This handles the event where an entity is damaged. If the entity is a player, and is on the grace period,
+     * the event is canceled (damage is not received), and the victim and attacker (if applicable) are messaged.
+     *
+     * If the attacking entity is a Player and is on Grace Period, the grace period is revoked.
+     *
      * @param event The event where an Entity receives damage
      */
     @EventHandler
@@ -68,24 +142,32 @@ public final class DeathGracePeriod extends JavaPlugin implements Listener {
                 //invincibleTime is converted from ticks to milliseconds, as (1/20) * 1000 = 50
                 long timeLeft = (invincibleTime *  50) - (System.currentTimeMillis() - timeAtRespawn);
 
-                //invincible time is converted to seconds to tell the player how much longer they are invulnerable
+                //invincibleTime is converted to seconds to tell the player how much longer they are invulnerable
                 p.sendMessage(prefix + "You are protected from damage for "
                         + ChatColor.GREEN + (timeLeft / 1000)
                         + ChatColor.RESET + " seconds!");
+            }
 
-                if(event instanceof EntityDamageByEntityEvent) {
-                    EntityDamageByEntityEvent damageByEntityEvent = (EntityDamageByEntityEvent) event;
+            if(event instanceof EntityDamageByEntityEvent) {
+                EntityDamageByEntityEvent damageByEntityEvent = (EntityDamageByEntityEvent) event;
+                Entity enemy = damageByEntityEvent.getDamager();
 
-                    Entity enemy = damageByEntityEvent.getDamager();
+                if(enemy instanceof Player) {
+                    Player damager = (Player) enemy;
+                    String damagerUUID = damager.getUniqueId().toString();
 
-                    if(enemy instanceof Player) {
-                        Player damager = (Player) enemy;
+                    if(event.isCancelled()) {
                         damager.sendMessage(prefix + "This player can't take damage yet!");
                     }
-                }
 
-            } else {
-                this.getLogger().info("Player is NOT on grace period");
+                    if(playerHasGracePeriod(damagerUUID)) {
+                        event.setCancelled(true);
+                        this.getLogger().info("Attacker was on grace period");
+
+                        cancelGraceEndingTask(damagerUUID);
+                        damager.sendMessage(prefix + "You are no longer invulnerable because you attacked a player!");
+                    }
+                }
             }
         }
     }
@@ -108,9 +190,32 @@ public final class DeathGracePeriod extends JavaPlugin implements Listener {
         playersOnGracePeriod.put(playerUUID, timeAtRespawn);
         p.sendMessage(prefix + "You are now invulnerable for " + ChatColor.GREEN + (invincibleTime / 20) + ChatColor.RESET + " seconds!");
 
-        Bukkit.getScheduler().runTaskLater(this, () -> {
+        int taskID = Bukkit.getScheduler().runTaskLater(this, () -> {
             playersOnGracePeriod.remove(playerUUID);
             p.sendMessage(prefix + "You are no longer on grace period!");
-        }, invincibleTime);
+        }, invincibleTime).getTaskId();
+
+        playerGraceEndingTask.put(playerUUID, taskID);
+    }
+
+    /**
+     * Remove the entry using the UUID key from both HashMaps.
+     * Used to prematurely remove a player from the Grace Period.
+     * The BukkitTask associated with the player's grace period must be canceled.
+     *
+     * @param UUID The player's UUID, the key used in the HashMaps
+     */
+    private void cancelGraceEndingTask(String UUID){
+        playersOnGracePeriod.remove(UUID);
+        Bukkit.getServer().getScheduler().cancelTask(playerGraceEndingTask.remove(UUID));
+    }
+
+    /**
+     *
+     * @param UUID String representation of a Player's UUID
+     * @return true if player has a grace period, false otherwise
+     */
+    private boolean playerHasGracePeriod(String UUID) {
+        return playersOnGracePeriod.containsKey(UUID);
     }
 }
